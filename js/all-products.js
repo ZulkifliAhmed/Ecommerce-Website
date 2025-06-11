c356d2de87dc2530643c5498d1778bde010b0fcf
ZulkifliAhmed <zoalkeflabunday@gmail.com>
2025-06-11 19:43:44 +0300

    Resopnsive All Products

---
+++ js/all-products.js
@@ -0,0 +1,40 @@
+fetch("js/items.json")
+  .then((Response) => Response.json())
+  .then((products) => {
+    const allProducts = document.querySelector("#products-div");
+    productsInCart = products;
+
+    products.forEach((products) => {
+        const oldPrice = products.old_price ? "$"+products.old_price :'';
+        const Discount =  products.old_price ? `<span class="sale-present">%${Math.floor(((products.old_price - products.price) / products.old_price) * 100)}</span>` : '';
+        allProducts.innerHTML += `
+        <div class="product swiper-slide">
+          ${Discount}
+            <div class="icons">
+                <span onclick="addItemsInCart(${products.id}, this)" id="cart-icon" class="cart-icon"><i class="fa-solid fa-cart-plus"></i></span>
+                <span class="cart-icon"><i class="fa-solid fa-heart"></i></span>
+                <span class="cart-icon"><i class="fa-solid fa-share"></i></span>
+            </div>
+            <div class="img-product">
+                <img src="${products.img}"
+                    alt="${products.name}">
+                <img src="${products.img_hover}" alt="${products.name}"
+                    class="img-hover">
+            </div>
+            <h3 class="name-product"><a href="#">${products.name}</a></h3>
+            <div class="stars">
+               <i class="fa-solid fa-star"></i>
+               <i class="fa-solid fa-star"></i>
+               <i class="fa-solid fa-star"></i>
+               <i class="fa-solid fa-star"></i>
+               <i class="fa-solid fa-star"></i>
+            </div>
+            <div class="price">
+                <p><span>$${products.price}</span></p>
+                <div class="old-price">${oldPrice}</div>
+            </div>
+        </div>
+    `;
+      
+    });
+  });
